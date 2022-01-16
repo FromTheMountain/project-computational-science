1abc25ee1a4474395c8ce919418cafb3fdef87dd
djakkalap <thedoge2706@gmail.com>
2022-01-16 23:40:13 +0100

    Refactor the code to use a class

--- LBM.py
+++ LBM.py
@@ -1,103 +1,104 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import time
 
-NUM_LOOPS = 3
-LATTICE_WIDTH = 200
-LATTICE_HEIGHT = 200
+# Model
+ITERATIONS = 500
+
+# LBM parameters
 c = np.array([(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1),
               (-1, 1), (-1, -1), (1, -1)])
 w = np.array([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
 
-# Some alternative velocity sets for testing:
-
-# c = np.array([(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1)])
-# w = np.array([4/9, 1/9, 1/9, 1/9, 1/9])
-
-# c = np.array([(0, 0), (1, 0)])
-# w = np.array([4/9, 1/9])
-
-NUM_VELOCITIES = len(c)
-cssq = 1/3
-
-# delta_t was chosen to be 1
-# omega was taken as defined in 03_karman_vortex.py
-
-viscosity = 0.02
-tau = 3*viscosity + 0.5
-
-def moment_update(f):
-    rho = np.sum(f, 2)
-    u_rho = np.repeat(np.repeat(np.sum(c, 0)[np.newaxis,:], LATTICE_WIDTH, 0)[np.newaxis,:], LATTICE_HEIGHT, 0)
-    u = u_rho / np.repeat(rho[:,:,np.newaxis], 2, 2)
-
-    return rho, u
+viscosity = 0.01
+TAU = 3*viscosity + 0.5
+DELTA_T = 1
+DELTA_X = 1
+NUM_LOOPS = 3
+LATTICE_WIDTH = 200
+LATTICE_HEIGHT = 200
+Q = 9
+cssq = (1/3) * (DELTA_X / DELTA_T)**2
 
 
-def get_equilibrium(rho, u):
-    f_eq = np.zeros((LATTICE_WIDTH, LATTICE_HEIGHT, 9), dtype=float)
-    udotu = np.repeat(np.einsum("xyk,xyk->xy", u, u)[:,:,np.newaxis], 9, 2) # CORRECT
-    udotc = np.einsum("xyk, vk -> xyv", u, c) # CORRECT
-    w_rho = np.einsum("k, xy -> xyk", w, rho) # CORRECT
+class LBM:
+    def __init__(self):
+        self.snapshots = []
 
-    f_eq = w_rho * (1 + (udotc / cssq) + (udotc**2 / (2 * cssq**2)) - (udotu / (2 * cssq)))
+    """Initialise and run the simulation.
+    """
+    def run(self):
+        f = np.random.random([LATTICE_WIDTH, LATTICE_HEIGHT, Q])
 
-    return f_eq
+        for _ in range(ITERATIONS):
+            f, rho, u = LBM.lbm_iteration(f)
 
+            self.snapshots.append(rho)
 
-def collision(f, f_eq):
-    f = f * (1 - (1 / tau)) + (1 / tau) * f_eq
+    """Update the rho and velocity values of each grid position.
+    """
+    def moment_update(f):
+        rho = np.sum(f, 2)
+        u_rho = np.repeat(np.repeat(np.sum(c, 0)[np.newaxis,:], LATTICE_WIDTH, 0)[np.newaxis,:], LATTICE_HEIGHT, 0)
+        u = u_rho / np.repeat(rho[:,:,np.newaxis], 2, 2)
 
-    return f
+        return rho, u
 
-def streaming(f):
-    for i in range(NUM_VELOCITIES):
-        f[:, :, i] = np.roll(f[:, :, i], c[i], axis=(1, 0))
+    """Calculate the equalibrium values of the grid.
+    """
+    def get_equilibrium(rho, u):
+        f_eq = np.zeros((LATTICE_WIDTH, LATTICE_HEIGHT, 9), dtype=float)
+        udotu = np.repeat(np.einsum("xyk,xyk->xy", u, u)[:,:,np.newaxis], 9, 2)
+        udotc = np.einsum("xyk, vk -> xyv", u, c)
+        w_rho = np.einsum("k, xy -> xyk", w, rho)
 
-    return f
+        f_eq = w_rho * (1 + (udotc / cssq) + (udotc**2 / (2 * cssq**2)) - (udotu / (2 * cssq)))
 
+        return f_eq
 
-def lbm_iteration(f):
-    # moment update
-    rho, u = moment_update(f)
+    """Perform an iteration of the Lattice-Boltzmann method.
+    """
+    def lbm_iteration(f):
+        # moment update
+        rho, u = LBM.moment_update(f)
 
-    # # equilibrium
-    f_eq = get_equilibrium(rho, u)
+        # # equilibrium
+        f_eq = LBM.get_equilibrium(rho, u)
 
-    # # collision
-    f = collision(f, f_eq)
+        # # collision
+        f = f * (1 - (DELTA_T / TAU)) + (DELTA_T / TAU) * f_eq
 
-    # streaming
-    f = streaming(f)
+        # streaming
+        for i in range(Q):
+            f[:, :, i] = np.roll(f[:, :, i], c[i], axis=(1, 0))
 
-    return f, rho, u
-
-
-# initialisation
-# f = np.full([LATTICE_WIDTH, LATTICE_HEIGHT, NUM_VELOCITIES], 1.0)
-f = np.random.random([LATTICE_WIDTH, LATTICE_HEIGHT, NUM_VELOCITIES])
-
-rho, u = moment_update(f)
-
-fig, ax = plt.subplots()
-img = plt.imshow(rho, extent=(0, LATTICE_WIDTH, 0, LATTICE_HEIGHT),
-                 cmap=plt.get_cmap("Greys"))
-
-
-def init():
-    ax.set_xlim(0, LATTICE_WIDTH)
-    ax.set_ylim(0, LATTICE_HEIGHT)
-    return img,
+        return f, rho, u
 
 
-def update(frame):
-    global f
+"""Render the values collected by the model with matplotlib.
+"""
+def render_lbm_model(model, save=False):
+    fig, ax = plt.subplots()
+    img = plt.imshow(model.snapshots[0], extent=(0, LATTICE_WIDTH, 0, LATTICE_HEIGHT),
+                    cmap=plt.get_cmap("Greys"))
 
-    f, rho, _ = lbm_iteration(f)
-    img.set_data(rho)
-    return img,
+    def animate(i):
+        ax.set_title(i)
+        img.set_data(model.snapshots[i])
+
+    anim = FuncAnimation(fig, animate, interval=1, frames=len(model.snapshots) - 1,
+                            repeat=False)
+
+    plt.show()
+
+    if save:
+        anim.save(time.strftime("%Y%m%d-%H%M%S.gif"))
 
 
-anim = FuncAnimation(fig, update, frames=500, interval=1,
-                     init_func=init, repeat=False, blit=True)
-plt.show()
+if __name__ == '__main__':
+    model = LBM()
+
+    model.run()
+
+    render_lbm_model(model, True)
