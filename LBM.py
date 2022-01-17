c0353d30cf952ddee19a8dc56a3a63dd58e9279b
Jeroen <jeroenvdb@live.nl>
2022-01-17 16:25:02 +0100

    v2

--- LBM.py
+++ LBM.py
@@ -3,8 +3,14 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import time
 
+# The initial distribution function takes values from a normal distribution.
+# Large values for NORMAL_DIST_MEAN (>= 0.5, more or less) cause the
+# distribution function to explode; this behaviour does not show up with
+# smaller values.
+NORMAL_DIST_MEAN = 0.01
+
 # Model
-ITERATIONS = 500
+ITERATIONS = 200
 
 # LBM parameters
 c = np.array([(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1),
@@ -16,44 +22,55 @@ TAU = 3*viscosity + 0.5
 DELTA_T = 1
 DELTA_X = 1
 NUM_LOOPS = 3
-LATTICE_WIDTH = 200
-LATTICE_HEIGHT = 200
+LATTICE_WIDTH = 50
+LATTICE_HEIGHT = 50
 Q = 9
 cssq = (1/3) * (DELTA_X / DELTA_T)**2
 
 
 class LBM:
     def __init__(self):
-        self.snapshots = []
+        self.rho_snapshots = []
+        self.ux_snapshots = []
+        self.uy_snapshots = []
 
     """Initialise and run the simulation.
     """
     def run(self):
-        f = np.random.random([LATTICE_WIDTH, LATTICE_HEIGHT, Q])
+        f = np.ones((LATTICE_WIDTH,LATTICE_HEIGHT,Q))
+        f += NORMAL_DIST_MEAN * np.random.randn(LATTICE_WIDTH,LATTICE_HEIGHT,Q)
 
         for _ in range(ITERATIONS):
-            f, rho, u = LBM.lbm_iteration(f)
+            f, rho, ux, uy = LBM.lbm_iteration(f)
 
-            self.snapshots.append(rho)
+            self.rho_snapshots.append(rho)
+            self.ux_snapshots.append(ux)
+            self.uy_snapshots.append(uy)
 
     """Update the rho and velocity values of each grid position.
     """
     def moment_update(f):
         rho = np.sum(f, 2)
-        u_rho = np.repeat(np.repeat(np.sum(c, 0)[np.newaxis,:], LATTICE_WIDTH, 0)[np.newaxis,:], LATTICE_HEIGHT, 0)
-        u = u_rho / np.repeat(rho[:,:,np.newaxis], 2, 2)
 
-        return rho, u
+        ux = np.sum(c[:,0] * f, axis=2) / rho
+        uy = np.sum(c[:,1] * f, axis=2) / rho
+
+        return rho, ux, uy
 
     """Calculate the equalibrium values of the grid.
     """
-    def get_equilibrium(rho, u):
-        f_eq = np.zeros((LATTICE_WIDTH, LATTICE_HEIGHT, 9), dtype=float)
-        udotu = np.repeat(np.einsum("xyk,xyk->xy", u, u)[:,:,np.newaxis], 9, 2)
-        udotc = np.einsum("xyk, vk -> xyv", u, c)
-        w_rho = np.einsum("k, xy -> xyk", w, rho)
+    def get_equilibrium(rho, ux, uy):
+        udotu = ux * ux + uy * uy
 
-        f_eq = w_rho * (1 + (udotc / cssq) + (udotc**2 / (2 * cssq**2)) - (udotu / (2 * cssq)))
+        udotc = np.zeros([LATTICE_WIDTH, LATTICE_HEIGHT, Q])
+        for i in range(Q):
+            udotc[:,:,i] = ux * c[i,0] + uy * c[i,1]
+
+        f_eq = np.zeros((LATTICE_WIDTH, LATTICE_HEIGHT, 9), dtype=float)
+
+        for i in range(Q):
+            f_eq[:,:,i] = w[i] * rho * (1 + udotc[:,:,i] / cssq +
+                (udotc[:,:,i])**2 / (2 * cssq**2) - udotu / (2 * cssq))
 
         return f_eq
 
@@ -61,33 +78,33 @@ class LBM:
     """
     def lbm_iteration(f):
         # moment update
-        rho, u = LBM.moment_update(f)
+        rho, ux, uy = LBM.moment_update(f)
 
-        # # equilibrium
-        f_eq = LBM.get_equilibrium(rho, u)
+        # equilibrium
+        f_eq = LBM.get_equilibrium(rho, ux, uy)
 
-        # # collision
+        # collision
         f = f * (1 - (DELTA_T / TAU)) + (DELTA_T / TAU) * f_eq
 
         # streaming
         for i in range(Q):
             f[:, :, i] = np.roll(f[:, :, i], c[i], axis=(1, 0))
 
-        return f, rho, u
+        return f, rho, ux, uy
 
 
 """Render the values collected by the model with matplotlib.
 """
 def render_lbm_model(model, save=False):
     fig, ax = plt.subplots()
-    img = plt.imshow(model.snapshots[0], extent=(0, LATTICE_WIDTH, 0, LATTICE_HEIGHT),
+    img = plt.imshow(model.rho_snapshots[0], extent=(0, LATTICE_WIDTH, 0, LATTICE_HEIGHT),
                     cmap=plt.get_cmap("Greys"))
 
     def animate(i):
         ax.set_title(i)
-        img.set_data(model.snapshots[i])
+        img.set_data(model.rho_snapshots[i])
 
-    anim = FuncAnimation(fig, animate, interval=1, frames=len(model.snapshots) - 1,
+    anim = FuncAnimation(fig, animate, interval=10, frames=len(model.rho_snapshots) - 1,
                             repeat=False)
 
     plt.show()
@@ -101,4 +118,4 @@ if __name__ == '__main__':
 
     model.run()
 
-    render_lbm_model(model, True)
+    render_lbm_model(model)
