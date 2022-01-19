cef02636fc1e0684988da97f1e415c5348b71ebd
Jeroen van den Berg <jeroenvdb@live.nl>
2022-01-19 15:32:52 +0100

    better particle tracking

--- LBM.py
+++ LBM.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from scipy.interpolate import interp2d
+from scipy.interpolate import RegularGridInterpolator
 
 # The initial distribution function takes values from a normal distribution.
 # Large values for NORMAL_DIST_MEAN (>= 0.5, more or less) cause the
@@ -11,8 +11,9 @@ from scipy.interpolate import interp2d
 NORMAL_DIST_STDDEV = 0.3
 
 # Model
-ITERATIONS = 200
-SNAPSHOT_INTERVAL = 5
+ITERATIONS = 31
+SNAP_INTERVAL = 5
+SNAPSHOTS = (ITERATIONS - 1)//SNAP_INTERVAL + 1
 
 # LBM parameters
 c = np.array([(0, 0), (1, 0), (0, 1), (-1, 0), (0, -1), (1, 1),
@@ -24,31 +25,31 @@ TAU = 3*viscosity + 0.5
 DELTA_T = 1
 DELTA_X = 1
 NUM_LOOPS = 3
-LATTICE_WIDTH = 200
-LATTICE_HEIGHT = 200
+WIDTH = 10
+HEIGHT = 10
 Q = 9
 cssq = (1/3) * (DELTA_X / DELTA_T)**2
 
 
 class LBM:
     def __init__(self):
-        self.rho_snapshots = []
-        self.ux_snapshots = []
-        self.uy_snapshots = []
+        self.rho_snapshots = np.zeros((SNAPSHOTS, WIDTH, HEIGHT))
+        self.ux_snapshots = np.copy(self.rho_snapshots)
+        self.uy_snapshots = np.copy(self.rho_snapshots)
 
     """Initialise and run the simulation.
     """
     def run(self):
-        f = np.ones((LATTICE_WIDTH,LATTICE_HEIGHT,Q))
-        f += NORMAL_DIST_STDDEV * np.random.randn(LATTICE_WIDTH,LATTICE_HEIGHT,Q)
+        f = np.ones((WIDTH,HEIGHT,Q))
+        f += NORMAL_DIST_STDDEV * np.random.randn(WIDTH,HEIGHT,Q)
 
         for i in range(ITERATIONS):
             f, rho, ux, uy = LBM.lbm_iteration(f)
 
-            if i % SNAPSHOT_INTERVAL == 0:
-                self.rho_snapshots.append(rho)
-                self.ux_snapshots.append(ux)
-                self.uy_snapshots.append(uy)
+            if i % SNAP_INTERVAL == 0 or i == ITERATIONS - 1:
+                self.rho_snapshots[i//SNAP_INTERVAL] = rho
+                self.ux_snapshots[i//SNAP_INTERVAL] = ux
+                self.uy_snapshots[i//SNAP_INTERVAL] = uy
 
     """Update the rho and velocity values of each grid position.
     """
@@ -65,11 +66,11 @@ class LBM:
     def get_equilibrium(rho, ux, uy):
         udotu = ux * ux + uy * uy
 
-        udotc = np.zeros([LATTICE_WIDTH, LATTICE_HEIGHT, Q])
+        udotc = np.zeros([WIDTH, HEIGHT, Q])
         for i in range(Q):
             udotc[:,:,i] = ux * c[i,0] + uy * c[i,1]
 
-        f_eq = np.zeros((LATTICE_WIDTH, LATTICE_HEIGHT, 9), dtype=float)
+        f_eq = np.zeros((WIDTH, HEIGHT, 9), dtype=float)
 
         for i in range(Q):
             f_eq[:,:,i] = w[i] * rho * (1 + udotc[:,:,i] / cssq +
@@ -102,24 +103,26 @@ class LBM:
 """Render the values collected by the model with matplotlib.
 """
 def render_lbm_model(model, particle_locations, save=False):
+    particles = particle_locations.shape[1]
+
     fig, ax = plt.subplots()
-    rho_plot = plt.imshow(model.rho_snapshots[0], extent=(0, LATTICE_WIDTH, 0, LATTICE_HEIGHT),
+    rho_plot = plt.imshow(model.rho_snapshots[0], extent=(0, WIDTH, 0, HEIGHT),
                     cmap=plt.get_cmap("Greys"))
 
     particle_plots = [plt.plot(particle_locations[0,i,0],
                                particle_locations[0,i,1],
                                'ro', markersize=10)[0]
-                      for i in range(4)]
+                      for i in range(particles)]
 
     def animate(i):
         ax.set_title(i)
-        rho_plot.set_data(model.rho_snapshots[i//SNAPSHOT_INTERVAL])
+        rho_plot.set_data(model.rho_snapshots[i//SNAP_INTERVAL])
 
-        for j in range(4):
+        for j in range(particles):
             particle_plots[j].set_data(particle_locations[i,j,0],
                                        particle_locations[i,j,1])
 
-    anim = FuncAnimation(fig, animate, interval=200, frames=ITERATIONS,
+    anim = FuncAnimation(fig, animate, interval=300, frames=ITERATIONS,
                          repeat=False)
 
     plt.show()
@@ -130,37 +133,34 @@ def render_lbm_model(model, particle_locations, save=False):
 
 def track_particles(model):
     """
-    TrackS the motions of four particles through the airflow.
+    Tracks the motions of particles through the airflow.
     """
-    num_particles = 4
+    gridsize = 2
 
-    particle_locations = np.zeros((ITERATIONS, num_particles, 2))
-    particle_locations[0] = np.array([
-        [LATTICE_WIDTH / 3, LATTICE_HEIGHT / 3],
-        [LATTICE_WIDTH / 3, 2 * LATTICE_HEIGHT / 3],
-        [2 * LATTICE_WIDTH / 3, LATTICE_HEIGHT / 3],
-        [2 * LATTICE_WIDTH / 3, 2 * LATTICE_HEIGHT / 3]
-    ])
+    particle_locations = np.zeros((ITERATIONS, gridsize**2, 2))
+    xs = ys = np.linspace((1/(gridsize + 1)) * WIDTH, 
+                          (1 - 1/(gridsize + 1)) * WIDTH, gridsize)
+    particle_locations[0] = np.dstack(np.meshgrid(xs, ys)).reshape(-1, 2)
+
+    ux_func = RegularGridInterpolator((np.arange(0, ITERATIONS, SNAP_INTERVAL),
+                                       np.arange(WIDTH), np.arange(HEIGHT)),
+                                      model.ux_snapshots)
+
+    uy_func = RegularGridInterpolator((np.arange(0, ITERATIONS, SNAP_INTERVAL),
+                                       np.arange(WIDTH), np.arange(HEIGHT)),
+                                      model.uy_snapshots)
 
     for i in range(ITERATIONS - 1):
-        if i % SNAPSHOT_INTERVAL == 0:
-            # Get linear interpolation function for ux and uy
-            ux_func = interp2d(np.arange(LATTICE_WIDTH),
-                               np.arange(LATTICE_HEIGHT),
-                               model.ux_snapshots[i//SNAPSHOT_INTERVAL])
-            uy_func = interp2d(np.arange(LATTICE_WIDTH),
-                               np.arange(LATTICE_HEIGHT),
-                               model.uy_snapshots[i//SNAPSHOT_INTERVAL])
-
         # Add the linearly interpolated velocity vector to the location of the
         # point.
-        for j in range(num_particles):
+        for j in range(gridsize**2):
             x, y = particle_locations[i,j]
-            dx, dy = ux_func(x, y)[0], uy_func(x, y)[0]
-            x += dx
-            y += dy
+            dx, dy = ux_func([i, x, y])[0], uy_func([i, x, y])[0]
 
-            particle_locations[i+1, j] = [x, y]
+            # Keep particles inside boundaries
+            new_x = min(max(0, x + dx), WIDTH - 1)
+            new_y = min(max(0, y + dy), HEIGHT - 1)
+            particle_locations[i+1, j] = [new_x, new_y]
 
     return particle_locations
 
@@ -172,4 +172,3 @@ if __name__ == '__main__':
     particle_locations = track_particles(model)
 
     render_lbm_model(model, particle_locations)
-
