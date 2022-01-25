ae6cc4bf4bfacb011a07d91425f8e0d460143117
Jeroen <jeroenvdb@live.nl>
2022-01-25 20:35:36 +0100

    read maps from file, rewrote part. tracking

--- LBM.py
+++ LBM.py
@@ -11,7 +11,7 @@ from scipy.interpolate import RegularGridInterpolator
 NORMAL_DIST_STDDEV = 0.3
 
 # Model
-ITERATIONS = 1601
+ITERATIONS = 3201
 SNAP_INTERVAL = 1
 SNAPSHOTS = (ITERATIONS - 1)//SNAP_INTERVAL + 1
 
@@ -27,49 +27,68 @@ DELTA_X = 1
 Q = 9
 cssq = (1/3) * (DELTA_X / DELTA_T)**2
 
-# # Karman vortex
-# WIDTH = 200
-# HEIGHT = 80
-# wall = np.zeros((WIDTH, HEIGHT), bool)
+WALL = 1
+INLET = 2
+OUTLET = 3
 
-# for y in range(0, HEIGHT):
-#     for x in range(0, WIDTH):
-#         if np.sqrt((x-WIDTH/4)**2 + (y-HEIGHT/2)**2) < 10.0:
-#             wall[x, y] = True
 
-# wall[198:, :] = True
-# Lid driven cavity
-WIDTH = 100
-HEIGHT = 100
-wall = np.zeros((WIDTH, HEIGHT), bool)
-wall[0, :] = True
-wall[:, 0] = True
-wall[-1, :] = True
+def read_map_from_file(filename):
+    with open(filename, 'r') as f:
+        iterator = enumerate(f)
 
-# wall[WIDTH//3:(2 * WIDTH//3), HEIGHT//3:(2*HEIGHT//3)] = True
+        _, firstline = next(iterator)
+        width, height = [int(x) for x in firstline.strip().split(',')]
+
+        wall = np.zeros((width, height), bool)
+        inlet = np.zeros((width, height), bool)
+        outlet = np.zeros((width, height), bool)
+
+        for i, line in iterator:
+            for j, c in enumerate(line.strip()):
+                c = int(c)
+                if c == WALL:
+                    wall[j, width-i] = True
+                elif c == INLET:
+                    inlet[j, width-i] = True
+                elif c == OUTLET:
+                    outlet[j, width-i] = True
+
+    return wall, inlet, outlet
 
 
 class LBM:
-    def __init__(self):
-        self.rho_snapshots = np.zeros((SNAPSHOTS, WIDTH, HEIGHT))
+    def __init__(self, wall, inlet, outlet):
+        # Get the map details
+        assert wall.shape == inlet.shape == outlet.shape
+        self.width, self.height = wall.shape
+
+        self.wall = wall
+        self.inlet = inlet
+        self.outlet = outlet
+
+        # Set the initial macroscopic quantities
+        self.rho = np.ones((self.width, self.height))
+        # self.rho += 0.05 * np.random.randn(WIDTH, HEIGHT)
+        self.ux = np.full((self.width, self.height), 0.0)
+
+        # Lid driven cavity
+        # self.ux[:, -2:] = 0.3
+
+        self.uy = np.zeros((self.width, self.height))
+
+        self.f = LBM.get_equilibrium(self.width * self.height,
+                                     self.rho.flatten(), self.ux.flatten(),
+                                     self.uy.flatten()).reshape(
+            (self.width, self.height, Q))
+
+        self.rho_snapshots = np.zeros((SNAPSHOTS, self.width, self.height))
         self.ux_snapshots = np.copy(self.rho_snapshots)
         self.uy_snapshots = np.copy(self.rho_snapshots)
 
-        self.rho = np.ones((WIDTH, HEIGHT))
-        # self.rho += 0.05 * np.random.randn(WIDTH, HEIGHT)
-        self.ux = np.full((WIDTH, HEIGHT), 0.0)
-
-        # Lid driven cavity
-        self.ux[:, -2:] = 0.3
-
-        self.uy = np.zeros((WIDTH, HEIGHT))
-
-        self.f = LBM.get_equilibrium(self.rho, self.ux, self.uy)
-
-    """Initialise and run the simulation.
-    """
-
     def run(self):
+        """
+        Initialise and run the simulation.
+        """
         for i in range(ITERATIONS):
             self.lbm_iteration(i)
 
@@ -78,9 +97,10 @@ class LBM:
                 self.ux_snapshots[i//SNAP_INTERVAL] = self.ux
                 self.uy_snapshots[i//SNAP_INTERVAL] = self.uy
 
-    """Update the rho and velocity values of each grid position.
-    """
     def moment_update(f):
+        """
+        Update the rho and velocity values of each grid position.
+        """
         rho = np.sum(f, 2)
 
         ux = np.sum(c[:, 0] * f, axis=2) / rho
@@ -88,33 +108,37 @@ class LBM:
 
         return rho, ux, uy
 
-    """Calculate the equalibrium values of the grid.
-    """
-    def get_equilibrium(rho, ux, uy, width=WIDTH, height=HEIGHT):
+    def get_equilibrium(n, rho, ux, uy):
+        """
+        Calculate the equalibrium distribution for the BGK operator.
+        """
         udotu = ux * ux + uy * uy
 
-        udotc = np.zeros([width, height, Q])
+        udotc = np.zeros((n, Q), dtype=float)
         for i in range(Q):
-            udotc[:, :, i] = ux * c[i, 0] + uy * c[i, 1]
+            udotc[:, i] = ux * c[i, 0] + uy * c[i, 1]
 
-        f_eq = np.zeros((width, height, 9), dtype=float)
+        f_eq = np.zeros((n, 9), dtype=float)
 
         for i in range(Q):
-            f_eq[:, :, i] = w[i] * rho * (1 + udotc[:, :, i] / cssq +
-                                          (udotc[:, :, i])**2 / (2 * cssq**2) -
-                                          udotu / (2 * cssq))
+            f_eq[:, i] = w[i] * rho * (1 + udotc[:, i] / cssq +
+                                       (udotc[:, i])**2 / (2 * cssq**2) -
+                                       udotu / (2 * cssq))
 
         return f_eq
 
-    """Perform an iteration of the Lattice-Boltzmann method.
-    """
-
     def lbm_iteration(self, it):
+        """
+        Perform an iteration of the Lattice-Boltzmann method.
+        """
         # moment update
         self.rho, self.ux, self.uy = LBM.moment_update(self.f)
 
         # equilibrium
-        f_eq = LBM.get_equilibrium(self.rho, self.ux, self.uy)
+        f_eq = LBM.get_equilibrium(self.width * self.height,
+                                   self.rho.flatten(), self.ux.flatten(),
+                                   self.uy.flatten()).reshape(
+                                       (self.width, self.height, Q))
 
         # Check stability condition
         assert np.min(f_eq) >= 0, "Simulation violated stability condition"
@@ -131,126 +155,138 @@ class LBM:
         boundary_f = boundary_f[:, [0, 3, 4, 1, 2, 7, 8, 5, 6]]
         self.f[wall, :] = boundary_f
 
-        # Karman vortex
-        # u0 = 0.0
-        # self.f[:10, :, 1] = 1/9 * (1 + 3*u0 + 4.5*u0**2 - 1.5*u0**2)
-        # self.f[:10, :, 3] = 1/9 * (1 - 3*u0 + 4.5*u0**2 - 1.5*u0**2)
-        # self.f[:10, :, 5] = 1/36 * (1 + 3*u0 + 4.5*u0**2 - 1.5*u0**2)
-        # self.f[:10, :, 8] = 1/36 * (1 + 3*u0 + 4.5*u0**2 - 1.5*u0**2)
-        # self.f[:10, :, 6] = 1/36 * (1 - 3*u0 + 4.5*u0**2 - 1.5*u0**2)
-        # self.f[:10, :, 7] = 1/36 * (1 - 3*u0 + 4.5*u0**2 - 1.5*u0**2)
+        # Set the velocity vector at inlets
+        inlet_ux = 0.2
+        inlet_uy = 0.0
+        inlet_rho = self.rho[self.inlet]
 
-        # Lid driven cavity
-        ux_top = 0.2
-        uy_top = 0
-        rho_top = self.rho[:, -2:]
+        self.f[self.inlet] = LBM.get_equilibrium(len(inlet_rho),
+                                                 self.rho[self.inlet],
+                                                 inlet_ux, inlet_uy)
 
-        self.f[:, -2:] = LBM.get_equilibrium(rho_top, ux_top, uy_top,
-                                             width=WIDTH, height=2)
+        # Set the density at outlets
+        outlet_rho = 0.9
+        outlet_ux = self.ux[self.outlet]
+        outlet_uy = self.uy[self.outlet]
+        self.f[self.outlet] = LBM.get_equilibrium(len(outlet_ux), outlet_rho,
+                                                  outlet_ux, outlet_uy)
 
+    def render(self, particle_locations=None, kind="density",
+               vectors=False, save=False):
+        """
+        Render the values collected by the model with matplotlib. Argument
+        "kind" should be of value "density" or "mag"
+        """
+        if particle_locations is not None:
+            particles = particle_locations.shape[1]
 
-def render_lbm_model(model, particle_locations=None, kind="density",
-                     vectors=False, save=False):
-    """
-    Render the values collected by the model with matplotlib. Argument "kind"
-    should be of value "density", "mag", or "vector"
-    """
-    # particles = particle_locations.shape[1]
+        fig, ax = plt.subplots()
 
-    fig, ax = plt.subplots()
-
-    init_vals = np.sqrt(model.ux_snapshots[0]**2 +
-                        model.uy_snapshots[0]**2) if kind == "mag" \
-        else model.rho_snapshots[0]
-    vmin = 0 if kind == "mag" else 0.8
-    vmax = 0.2 if kind == "mag" else 1.2
-    fluid_plot = plt.imshow(np.flip(init_vals.T, axis=0),
-                            extent=(0, WIDTH, 0, HEIGHT), vmin=vmin, vmax=vmax,
-                            cmap=plt.get_cmap("jet"))
-    plt.colorbar(fluid_plot)
-
-
-    if vectors:
-        x, y = np.meshgrid(np.linspace(0, WIDTH-1, 20, dtype=int),
-                           np.linspace(0, HEIGHT-1, 20, dtype=int))
-        u = model.ux_snapshots[0, x, y]
-        v = model.uy_snapshots[0, x, y]
-
-        vector_plot = plt.quiver(x, y, u, v)
-
-    # particle_plots = [plt.plot(particle_locations[0, i, 0] + 1/2,
-    #                            particle_locations[0, i, 1] + 1/2,
-    #                            'ro', markersize=10)[0]
-    #                   for i in range(particles)]
-
-    def animate(i):
-        ax.set_title("{}, iteration {}".format(kind, i))
-
-        vals = np.sqrt(model.ux_snapshots[i//SNAP_INTERVAL]**2 +
-                       model.uy_snapshots[i//SNAP_INTERVAL]**2) \
-            if kind == "mag" else model.rho_snapshots[i//SNAP_INTERVAL]
-
-        fluid_plot.set_data(np.flip(vals.T, axis=0))
+        init_vals = np.sqrt(model.ux_snapshots[0]**2 +
+                            model.uy_snapshots[0]**2) if kind == "mag" \
+            else model.rho_snapshots[0]
+        vmin = 0 if kind == "mag" else 0.8
+        vmax = 0.2 if kind == "mag" else 1.2
+        fluid_plot = plt.imshow(init_vals.T, origin="lower", vmin=vmin,
+                                vmax=vmax, cmap=plt.get_cmap("jet"))
+        plt.colorbar(fluid_plot)
 
         if vectors:
-            u = model.ux_snapshots[i//SNAP_INTERVAL, x, y]
-            v = model.uy_snapshots[i//SNAP_INTERVAL, x, y]
+            x, y = np.meshgrid(np.linspace(0, self.width-1, 20, dtype=int),
+                               np.linspace(0, self.height-1, 20, dtype=int))
+            u = model.ux_snapshots[0, x, y]
+            v = model.uy_snapshots[0, x, y]
 
-            vector_plot.set_UVC(u, v)
+            # Set scale to 0.5 for lid driven cavity, 4 for Karman vortex
+            vector_plot = plt.quiver(x, y, u, v, scale=0.5)
 
-        # for j in range(particles):
-        #     particle_plots[j].set_data(particle_locations[i, j, 0] + 1/2,
-        #                                particle_locations[i, j, 1] + 1/2)
+        if particle_locations is not None:
+            particle_plots = [plt.plot(particle_locations[0, i, 0] + 1/2,
+                                       particle_locations[0, i, 1] + 1/2,
+                                       'ro', markersize=10)[0]
+                              for i in range(particles)]
 
-    anim = FuncAnimation(fig, animate, interval=1, frames=ITERATIONS,
-                         repeat=True)
+        def animate(i):
+            ax.set_title("{}, iteration {}".format(kind, i))
 
-    plt.show()
+            vals = np.sqrt(model.ux_snapshots[i//SNAP_INTERVAL]**2 +
+                           model.uy_snapshots[i//SNAP_INTERVAL]**2) \
+                if kind == "mag" else model.rho_snapshots[i//SNAP_INTERVAL]
 
-    if save:
-        anim.save(time.strftime("%Y%m%d-%H%M%S.gif"))
+            fluid_plot.set_data(vals.T)
 
+            if vectors:
+                u = model.ux_snapshots[i//SNAP_INTERVAL, x, y]
+                v = model.uy_snapshots[i//SNAP_INTERVAL, x, y]
 
-def track_particles(model):
-    """
-    Tracks the motions of particles through the airflow.
-    """
-    gridsize = 6
+                vector_plot.set_UVC(u, v)
 
-    particle_locations = np.zeros((ITERATIONS, gridsize**2, 2))
+            if particle_locations is not None:
+                for j in range(particles):
+                    particle_plots[j].set_data(
+                        particle_locations[i, j, 0] + 1/2,
+                        particle_locations[i, j, 1] + 1/2)
 
-    fracs = np.linspace((1/(gridsize + 1)), (1 - 1/(gridsize + 1)), gridsize)
-    xs, ys = fracs * WIDTH, fracs * HEIGHT
-    particle_locations[0] = np.dstack(np.meshgrid(xs, ys)).reshape(-1, 2)
+        anim = FuncAnimation(fig, animate, interval=1, frames=ITERATIONS,
+                             repeat=True)
 
-    ux_func = RegularGridInterpolator((np.arange(0, ITERATIONS, SNAP_INTERVAL),
-                                       np.arange(WIDTH), np.arange(HEIGHT)),
-                                      model.ux_snapshots)
+        plt.show()
 
-    uy_func = RegularGridInterpolator((np.arange(0, ITERATIONS, SNAP_INTERVAL),
-                                       np.arange(WIDTH), np.arange(HEIGHT)),
-                                      model.uy_snapshots)
+        if save:
+            anim.save(time.strftime("%Y%m%d-%H%M%S.gif"))
 
-    for i in range(ITERATIONS - 1):
-        # Add the linearly interpolated velocity vector to the location of the
-        # point.
-        for j in range(gridsize**2):
-            x, y = particle_locations[i, j]
-            dx, dy = ux_func([i, x, y])[0], uy_func([i, x, y])[0]
+    def track_particles(self):
+        """
+        Tracks the motions of particles through the airflow.
+        """
+        num_particles = 20
 
-            # Keep particles inside boundaries
-            new_x = min(max(0, x + dx), WIDTH - 1)
-            new_y = min(max(0, y + dy), HEIGHT - 1)
-            particle_locations[i+1, j] = [new_x, new_y]
+        # Spawn num_particles particles at evenly spaced intervals.
+        particle_locations = np.zeros((ITERATIONS, num_particles, 2))
 
-    return particle_locations
+        ux_func = RegularGridInterpolator(
+            (np.arange(0, ITERATIONS, SNAP_INTERVAL), np.arange(self.width),
+             np.arange(self.height)), model.ux_snapshots)
+
+        uy_func = RegularGridInterpolator(
+            (np.arange(0, ITERATIONS, SNAP_INTERVAL), np.arange(self.width),
+             np.arange(self.height)), model.uy_snapshots)
+
+        for i in range(ITERATIONS - 1):
+            if i % (ITERATIONS // num_particles) == 0:
+                # Spawn a new particle
+                # Randomly choose an inlet cell.
+                inlet_indices = np.where(model.inlet)
+                idx = np.random.randint(len(inlet_indices[0]))
+
+                particle_locations[i, i // (ITERATIONS // num_particles)] = \
+                    inlet_indices[0][idx], inlet_indices[1][idx]
+
+            # Add the linearly interpolated velocity vector to the location of
+            # the point.
+            for j in range(i // (ITERATIONS // num_particles) + 1):
+                x, y = particle_locations[i, j]
+                dx, dy = ux_func([i, x, y])[0], uy_func([i, x, y])[0]
+
+                # Keep particles inside boundaries
+                new_x = min(max(0, x + dx), self.width - 1)
+                new_y = min(max(0, y + dy), self.height - 1)
+                particle_locations[i+1, j] = [new_x, new_y]
+
+        return particle_locations
 
 
 if __name__ == '__main__':
-    model = LBM()
+    # To change from lid driven cavity to Karman vortex, only two changes need
+    # to be made. First, the filename below needs to be modified to
+    # './maps/karmanvortex'. Second, the scale parameter in line 201 needs to
+    # be adjusted to 4.
+    wall, inlet, outlet = read_map_from_file('./maps/liddrivencavity')
 
+    model = LBM(wall, inlet, outlet)
     model.run()
 
-    # particle_locations = track_particles(model)
+    particle_locations = model.track_particles()
 
-    render_lbm_model(model, kind="mag", vectors=True)
+    model.render(kind="mag", particle_locations=particle_locations,
+                 vectors=True)
